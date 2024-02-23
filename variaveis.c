7a677b65a31290d5d5bb48198d114dd30b104914
JaderMach <jadermachado@live.com>
2024-02-23 17:17:37 -0300

    adicionando arquivo aula de variaveis

--- variaveis.c
+++ variaveis.c
@@ -1,21 +1,33 @@
 #include "stdio.h"
 
+int minhaIntGlobal = 19;
+
 void meuOutroMetodo(){
-    printf("Meu outro metodo foi executado");
+    printf("Meu outro metodo foi executado\n");
+    printf("%d\n", minhaIntGlobal);
 }
 
 int main(){
     char *c;
 
     int meuInt;
-    meuInt = 10;
+    meuInt = 11;
+    int a, b;
 
-    double meuDouble = 23.75;
+    double const meuDouble = 23.75;
     char meuChar = 'd';
+
+   // int register minhaVariavelRegister;
 
     printf("%d\n", meuInt);
     printf("%f\n", meuDouble);
     printf("%c\n", meuChar);
+
+    a = b = 11;
+
+    printf("%d\n", a);
+
+    printf("%d\n", minhaIntGlobal);
 
     meuOutroMetodo();
 
