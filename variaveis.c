fe03d1364fe7de28b6c0d09dc6da525cf07af225
JaderMach <jadermachado@live.com>
2024-02-23 17:34:37 -0300

    adicionando includes e defs da aula de variaveis

--- variaveis.c
+++ variaveis.c
@@ -1,3 +1,19 @@
+#define MINHA_DIR 3
+
+#ifdef MINHA_DIR
+    // ....
+#endif
+
+#if MINHA_DIR == 3
+    #define MINHA_DIR_2 2
+#elif MINHA_DIR == 4
+    #include "stdlib.h"
+#else
+    #define MINHA_DIR_3 3
+#endif
+
+#error funcoes inacabadas - jader machado
+
 #include "stdio.h"
 
 int minhaIntGlobal = 19;
@@ -16,12 +32,16 @@ int main(){
 
     double const meuDouble = 23.75;
     char meuChar = 'd';
+    float meuFloat = 10.0;
 
    // int register minhaVariavelRegister;
+
+    meuFloat = meuFloat * MINHA_DIR;
 
     printf("%d\n", meuInt);
     printf("%f\n", meuDouble);
     printf("%c\n", meuChar);
+    printf("%f\n", meuFloat);
 
     a = b = 11;
 
@@ -30,6 +50,9 @@ int main(){
     printf("%d\n", minhaIntGlobal);
 
     meuOutroMetodo();
+
+    printf("%d\n", MINHA_DIR_2);
+
 
     scanf("%c", &c);
     return 0;
